d38d77f3ea51118342353642508e97aeca6f8083
sashank malladi <sashank malladi>
2017-05-15 21:45:13 -0700

    job seeker search, filtering , apply, update fav

--- portal/src/main/java/com/Jobportal/Controllers/JobSearchRestController.java
+++ portal/src/main/java/com/Jobportal/Controllers/JobSearchRestController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +23,76 @@ import com.Jobportal.Service.JobSearchServices;
 
 @RestController
 public class JobSearchRestController {
-	
+
 	@Autowired
 	JobSearchServices _jobSearchService;
-	
-	@RequestMapping(value = "/jobs/{searchString}", method = RequestMethod.GET, produces = {
-			MediaType.APPLICATION_JSON_VALUE })
-	public List<JobPosition> getJobPositions(@PathVariable("searchString") String searchString) {
-		return _jobSearchService.findJobs(searchString);
+
+	@RequestMapping(value = "/jobs", method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public List<JobPosition> getJobPositions(@RequestBody Search search) {
+
+		if (search.getCompanyName().size() < 1) {
+			List<String> companyNames = search.getCompanyName();
+			companyNames.add("");
+			search.setCompanyName(companyNames);
+		}
+
+		if (search.getOfficeLocation().size() < 1) {
+			List<String> locations = search.getOfficeLocation();
+			locations.add("");
+			search.setOfficeLocation(locations);
+		}
+
+		return _jobSearchService.findJobs(search.getSearchString(), search.getCompanyName(), search.getOfficeLocation(),
+				search.getMinSal(), search.getMaxSal());
 	}
+}
+
+class Search {
+
+	private String searchString;
+	private List<String> companyName;
+	private List<String> officeLocation;
+	private String minSal;
+	private String maxSal;
+
+	public String getSearchString() {
+		return searchString;
+	}
+
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+
+	public List<String> getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(List<String> companyName) {
+		this.companyName = companyName;
+	}
+
+	public List<String> getOfficeLocation() {
+		return officeLocation;
+	}
+
+	public void setOfficeLocation(List<String> officeLocation) {
+		this.officeLocation = officeLocation;
+	}
+
+	public String getMinSal() {
+		return minSal;
+	}
+
+	public void setMinSal(String minSal) {
+		this.minSal = minSal;
+	}
+
+	public String getMaxSal() {
+		return maxSal;
+	}
+
+	public void setMaxSal(String maxSal) {
+		this.maxSal = maxSal;
+	}
+
 }
--- portal/src/main/resources/static/js/scripts1.js
+++ portal/src/main/resources/static/js/scripts1.js
@@ -11,154 +11,313 @@ var application = {
 	jobId : null
 }
 
-var app = angular.module('jobSeeker', [ 'ui.bootstrap' ]);
-app.controller('userProfile', function($scope, $modal, $http) {
-	$scope.init = function() {
-		$scope.fetchProfile();
-	};
+var app = angular.module('jobSeeker', [ 'ui.bootstrap', "ui.grid","checklist-model" ]);
+app
+		.controller(
+				'userProfile',
+				function($scope, $modal, $http) {
+					$scope.init = function() {
+						$scope.fetchProfile();
+					};
+					$scope.gridOptions1 = {
+						enableSorting : true,
+						appScopeProvider : $scope,
+						columnDefs : [
+								{
+									field : 'MyFav',
+									sortable : false,
+									cellTemplate : '<input type="checkbox" ng-model="row.entity.checked"></input>'
+								},
+								{
+									field : 'name'
+								},
+								{
+									field : 'title'
+								},
+								{
+									field : 'description'
+								},
+								{
+									field : 'responsibilities'
+								},
+								{
+									field : 'officeLocation'
+								},
+								{
+									field : 'salary'
+								},
+								{
+									field : 'Apply',
+									sortable : false,
+									cellTemplate : '<button class="btn btn-default"	ng-click="grid.appScope.showModal(row.entity)">Apply</button>'
+								} ],
 
-	$scope.fetchProfile = function() {
-		$http.get('http://localhost:8080/getseeker').then(function(response) {
-			var data = response.data;
-			$scope.profile.userId = data.userId;
-			$scope.profile.firstName = data.firstName;
-			$scope.profile.lastName = data.lastName;
-			$scope.profile.workExperience = data.workExperience;
-			$scope.profile.education = data.education;
-			// $scope.profile.selfIntroduction = data.selfIntroduction;
-			$scope.profile.skills = data.skills;
-			$scope.profile.picKey = data.picKey;
-			$scope.applications = data.applications;
-			var favJobs = {
+						onRegisterApi : function(gridApi) {
+							$scope.grid1Api = gridApi;
+						}
+					};
 
-			}
-			for ( var iterable_element in data.favoriteJobs) {
-				data.favoriteJobs[iterable_element].checked = true;
-			}
-			$scope.favoriteJobs = data.favoriteJobs;
-		});
+					$scope.fetchProfile = function() {
+						$http
+								.get('http://localhost:8080/getseeker')
+								.then(
+										function(response) {
+											var data = response.data;
+											$scope.profile.userId = data.userId;
+											$scope.profile.firstName = data.firstName;
+											$scope.profile.lastName = data.lastName;
+											$scope.profile.workExperience = data.workExperience;
+											$scope.profile.education = data.education;
+											// $scope.profile.selfIntroduction =
+											// data.selfIntroduction;
+											$scope.profile.skills = data.skills;
+											$scope.profile.picKey = data.picKey;
+											$scope.applications = data.applications;
+											var favJobs = {
 
-	};
+											}
+											for ( var iterable_element in data.favoriteJobs) {
+												data.favoriteJobs[iterable_element].checked = true;
+											}
+											$scope.favoriteJobs = data.favoriteJobs;
+										});
 
-	$scope.profile = {
-		userId : "",
-		firstName : "",
-		lastName : "",
-		workExperience : "",
-		education : "",
-		selfIntroduction : "",
-		skills : "",
-		picKey : ""
-	};
-	$scope.saveProfile = function() {
-		$http({
-			method : 'POST',
-			url : 'http://localhost:8080/updateProfile',
-			data : $scope.profile
-		}).then(function successCallback(response) {
-			if (response.data == 0) {
-				alert("Updated Successfully");
-			}
-		}, function errorCallback(response) {
-			alert("Error Occured");
-		});
+					};
 
-	};
-	$scope.saveFavinFav = function() {
+					$scope.profile = {
+						userId : "",
+						firstName : "",
+						lastName : "",
+						workExperience : "",
+						education : "",
+						selfIntroduction : "",
+						skills : "",
+						picKey : ""
+					};
+					$scope.favoriteJobs = [];
+					$scope.applications = [];
+					$scope.companiesFilter = [];
+					$scope.selectedCompaniesFilter = []; 
+					$scope.searchLocations =[]; 
+					$scope.selectedSearchLocations = []; 
+					$scope.minSalFilter=0;
+					$scope.maxSalFilter=0;
+					$scope.saveProfile = function() {
+						$http({
+							method : 'POST',
+							url : 'http://localhost:8080/updateProfile',
+							data : $scope.profile
+						}).then(function successCallback(response) {
+							if (response.data == 0) {
+								alert("Updated Successfully");
+							}
+						}, function errorCallback(response) {
+							alert("Error Occured");
+						});
 
-		var FavList = $scope.favoriteJobs.filter(function(position) {
-			return position.checked;
-		});
+					};
+					$scope.saveFavinFav = function() {
 
-		for ( var iterable_element in FavList) {
-			delete FavList[iterable_element].checked;
-		}
+						var FavList = $scope.favoriteJobs.filter(function(
+								position) {
+							return position.checked;
+						});
 
-		$http({
-			method : 'POST',
-			url : 'http://localhost:8080/updateFavfromFav',
-			data : FavList
-		}).then(function successCallback(response) {
-			if (response.data == 0) {
-				$scope.fetchProfile();
-				alert("Updated Successfully");
-			}
-		}, function errorCallback(response) {
-			alert("Error Occured");
-		});
+						for ( var iterable_element in FavList) {
+							delete FavList[iterable_element].checked;
+						}
 
-		// alert(FavList);
+						$http({
+							method : 'POST',
+							url : 'http://localhost:8080/updateFavfromFav',
+							data : FavList
+						}).then(function successCallback(response) {
+							if (response.data == 0) {
+								$scope.fetchProfile();
+								$scope.searchJobs();
+								alert("Updated Successfully");
+							}
+						}, function errorCallback(response) {
+							alert("Error Occured");
+						});
 
-	};
-	$scope.applyinFav = function(jobitem) {
+						// alert(FavList);
 
-		var application = {
-			status : "pending",
-			resumeKey : ResumeString,
-			jobPosition : jobitem
-		}
+					};
+					$scope.applyinFav = function(jobitem) {
 
-		$http({
-			method : 'POST',
-			url : 'http://localhost:8080/applyfromFav',
-			data : application
-		}).then(function successCallback(response) {
-			if (response.data == 0) {
-				$scope.fetchProfile();
-				alert("Updated Successfully");
-			}
-			return false;
-		}, function errorCallback(response) {
-			alert("Error Occured");
-		});
-		return false;
-	};
-	$scope.cancelApplied = function(jobitem) {
-		$http({
-			method : 'POST',
-			url : 'http://localhost:8080/cancelApplied',
-			data : jobitem
-		}).then(function successCallback(response) {
-			if (response.data == 0) {
-				$scope.fetchProfile();
-				alert("Updated Successfully");
-			}
-		}, function errorCallback(response) {
-			alert("Error Occured");
-		});
-		return false;
-	};
+						var application = {
+							status : "pending",
+							resumeKey : ResumeString,
+							jobPosition : jobitem
+						}
 
-	$scope.favoriteJobs = [];
-	$scope.applications = [];
-	$scope.showModal = function(jobitem) {
-		$scope.opts = {
-			backdrop : true,
-			backdropClick : true,
-			dialogFade : false,
-			keyboard : true,
-			templateUrl : 'popup.html',
-			controller : ModalInstanceCtrl,
-			resolve : {}
-		// empty storage
-		};
+						$http({
+							method : 'POST',
+							url : 'http://localhost:8080/applyfromFav',
+							data : application
+						}).then(function successCallback(response) {
+							if (response.data == 0) {
+								$scope.fetchProfile();
+								alert("Updated Successfully");
+							}
+							return false;
+						}, function errorCallback(response) {
+							alert("Error Occured");
+						});
+						return false;
+					};
+					$scope.cancelApplied = function(jobitem) {
+						$http({
+							method : 'POST',
+							url : 'http://localhost:8080/cancelApplied',
+							data : jobitem
+						}).then(function successCallback(response) {
+							if (response.data == 0) {
+								$scope.fetchProfile();
+								alert("Updated Successfully");
+							}
+						}, function errorCallback(response) {
+							alert("Error Occured");
+						});
+						return false;
+					};
 
-		$scope.opts.resolve.item = function() {
-			return $scope // pass name to Dialog
-		}
+					$scope.searchJobs = function() {
 
-		var modalInstance = $modal.open($scope.opts);
+					// var formData = $("#txtSearch").val();
+						
+						var SearchData={
+								searchString: $("#txtSearch").val(),
+								companyName:$scope.selectedCompaniesFilter,
+								officeLocation:$scope.selectedSearchLocations,
+								minSal:$scope.minSalFilter,
+								maxSal:$scope.maxSalFilter
+						}
+						if (SearchData != "") {
+							$http({
+								method : 'POST',
+								url : 'http://localhost:8080/jobs',
+								data : SearchData
+							})
+									.then(
+											function successCallback(response) {
 
-		modalInstance.result.then(function() {
-			// on ok button press
-			$scope.applyinFav(jobitem);
-		}, function() {
-			// on cancel button press
-			console.log("Modal Closed");
-		});
-	};
+												var companiesFilter = [];
+												var location = [];
 
-});
+												var positions = response.data
+														.map(function(result) {
+															result.name = result.company.name;
+															delete result.applications;
+															return result;
+														});
+
+												positions
+														.map(function(value) {
+															companiesFilter
+																	.push(value.name);
+															location
+																	.push(value.officeLocation);
+
+															$scope.favoriteJobs
+																	.map(function(
+																			value1) {
+																		if (value1.jobId == value.jobId) {
+																			value.checked = true;
+																		}
+																	})
+														});
+
+												$scope.gridOptions1.data = positions;
+												var abc = companiesFilter.filter((v, i, a) => a.indexOf(v) === i); 
+												$scope.companiesFilter=[];
+												abc.map(function(items){
+													$scope.companiesFilter.push(items);
+												})
+												$scope.searchLocations = location.filter((v, i, a) => a.indexOf(v) === i); 
+											},
+											function errorCallback(response) {
+												alert("Error Occured");
+											});
+						}
+						return false;
+					};
+
+					$scope.saveFavinSearch = function() {
+						var currentData = $scope.gridOptions1.data;
+						var FavList = currentData.filter(function(fav) {
+							return fav.checked;
+						});
+
+						currentData
+								.map(function(currentFav) {
+
+									if (currentFav.checked) {
+
+										var FavList = $scope.favoriteJobs
+												.filter(function(position) {
+													return position.jobId == currentFav.jobId;
+												});
+										if (FavList.length < 1) {
+											$scope.favoriteJobs
+													.push(currentFav);
+										}
+
+									} else {
+										var FavList = $scope.favoriteJobs
+												.filter(function(position) {
+													return position.jobId == currentFav.jobId;
+												});
+										if (FavList.length > 0) {
+
+											var curIndex = -1;
+											$scope.favoriteJobs
+													.map(function(val, index) {
+														if (val.jobId == currentFav.jobId) {
+															curIndex = index;
+														}
+													})
+
+											$scope.favoriteJobs.splice(
+													curIndex, 1);
+										}
+									}
+
+								})
+
+						$scope.saveFavinFav();
+
+					}
+
+					$scope.showModal = function(jobitem) {
+						$scope.opts = {
+							backdrop : true,
+							backdropClick : true,
+							dialogFade : false,
+							keyboard : true,
+							templateUrl : 'popup.html',
+							controller : ModalInstanceCtrl,
+							resolve : {}
+						// empty storage
+						};
+
+						$scope.opts.resolve.item = function() {
+							return $scope // pass name to Dialog
+						}
+
+						var modalInstance = $modal.open($scope.opts);
+
+						modalInstance.result.then(function() {
+							// on ok button press
+							$scope.applyinFav(jobitem);
+						}, function() {
+							// on cancel button press
+							console.log("Modal Closed");
+						});
+					};
+
+				});
 
 var ModalInstanceCtrl = function($scope, $modalInstance, $modal, item) {
 
